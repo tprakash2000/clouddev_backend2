8d06f68d1179f194b4bbe8746ff7cbca1322356f
AvantiKantumuchu <35277498+AvantiKantumuchu@users.noreply.github.com>
2025-04-09 12:11:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.json());
 // MySQL connection config
 // triggering azure deployment
 const db = mysql.createConnection({
-  host: 'mysqlclouddevavanti.mysql.database.azure.com',
-  user: 'mysqlAvanti',
-  password: 'Avanthi@123',
+  host: 'studentdb-demo.mysql.database.azure.com',
+  user: 'mysql_admin',
+  password: 'TempPass@123',
   database: 'studentdb',
   port: 3306,
   ssl: {
